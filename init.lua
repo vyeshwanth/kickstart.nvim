55d4d4374c9a2c4246b02771bb29e7388b583dd3
yeshwanth-personal <yeshwanth-personal@yeshwanths-MacBook-Pro-2.local>
2024-04-21 00:41:43 +0530

    telescope vertical layout

--- init.lua
+++ init.lua
@@ -388,11 +388,9 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          layout_strategy = 'vertical',
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
